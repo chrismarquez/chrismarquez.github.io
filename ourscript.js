8a91f330ce5827feca2e6cb999a7a190a039880b
KilluaZoldyck90 <a_h5n1@live.com>
2015-04-11 01:40:39 -0500

    commit adding functionality

---
+++ ourscript.js
@@ -0,0 +1,35 @@
+function when_click2() {
+	location.replace("index2.html");
+}
+
+function when_click1() {
+	location.replace("index.html");
+}
+
+function when_clicklogin() {
+	alert("We're working on this!");
+}
+
+function when_clicksignup() {
+	location.replace("index3.html");
+}
+
+function when_clickgetstarted() {
+	alert("Coming very soon! We're working on this!");
+}
+
+function setup() {
+	var click2 = $("#index2");
+	$(click2).click(when_click2);
+	var click1 = $(".taketoindex");
+	$(click1).click(when_click1);
+	var loginb = $("#login");
+	$(loginb).click(when_clicklogin);
+	var signupb = $(".signup");
+	$(signupb).click(when_clicksignup);
+	var getstartedb = $("#getstarted");
+	$(getstartedb).click(when_clickgetstarted);
+}
+
+$(document).ready(setup);
+
