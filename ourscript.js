6e27344a66531dffd0452115a4f8b378f4920c72
KilluaZoldyck90 <a_h5n1@live.com>
2015-04-25 21:56:05 -0500

    Added prototype

--- ourscript.js
+++ ourscript.js
@@ -6,13 +6,28 @@ function when_click1() {
 	location.replace("index.html");
 }
 
-
 function when_clicksignup() {
 	location.replace("index3.html");
 }
 
 function when_clickgetstarted() {
-	alert("Coming very soon! We're working on this!");
+	location.replace("menu1.html");
+}
+
+function when_clickoption() {
+	location.replace("menu2.html");
+}
+
+function when_clickoption2() {
+	location.replace("menu3.html");
+}
+
+function when_clickback1() {
+	location.replace("menu1.html");
+}
+
+function when_clickback2() {
+	location.replace("menu2.html");
 }
 
 function setup() {
@@ -24,6 +39,14 @@ function setup() {
 	$(signupb).click(when_clicksignup);
 	var getstartedb = $("#getstarted");
 	$(getstartedb).click(when_clickgetstarted);
+	var optionb = $(".option");
+	$(optionb).click(when_clickoption);
+	var back1 = $("#back1");
+	$(back1).click(when_clickback1);
+	var back2 = $("#back2");
+	$(back2).click(when_clickback2);
+	var option2 = $(".option2");
+	$(option2).click(when_clickoption2);
 }
 
 $(document).ready(setup);
